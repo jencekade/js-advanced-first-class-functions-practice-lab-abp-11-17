3757c4480cb604f3eeab747f6ff55702a6e23720
jencekade <gaephuoc@gmail.com>
2017-11-09 01:22:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+
+function logDriverNames (driver) {
+  let eachDriverName = driver.forEach(function (eachDriver){
+    console.log(eachDriver.name)
+  });
+  return eachDriverName;
+}
+
+function logDriversByHometown (driver, location) {
+  let driverLocation = driver.forEach(function(eachDriver){
+    if(eachDriver.hometown == location){
+      console.log(eachDriver.name);
+    }
+  });
+  return driverLocation;
+}
+
+function driversByRevenue (driver) {
+  let sortDriverRevenue = [...driver];
+  sortDriverRevenue = sortDriverRevenue.sort(function (a , b){
+    return a.revenue - b.revenue;
+  });
+  return sortDriverRevenue;
+}
+
+function driversByName (driver) {
+  let sortDriverName = [...driver];
+  sortDriverName = sortDriverName.sort(function (a, b){
+    let aName = a.name;
+    let bName = b.name;
+    return aName.localeCompare(bName);
+  });
+  return sortDriverName;
+}
